92e6f8efcbc4b441dbe3277140815adae97bf095
sprouttime <123569555+sprouttime@users.noreply.github.com>
2023-05-07 12:04:20 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,11 +13,27 @@ function progressLoad(){
 
     }
     per++
+    // while (per < 80) {
+    //     progress.style.width = `80%`;
+    //     percentage.innerHTML = "80%";
+    //     per++
+    // }
+    
 
 }
+function load(){   
+var loader = document.getElementById("pre-loader");
+var content = document.getElementById("content-cont");
+setInterval(()=>{
+    loader.style.display = "none";
+    content.style.position = "absolute";
+    content.style.transition = "4s"
+    content.style.opacity = "1";
 
+
+},4000)
+}
 setInterval(progressLoad,90);
-
 
 
 
@@ -32,6 +48,7 @@ function display(){
     nav.style.display = "none";
     if (nav.classList.toggle(nav.style.display = "none")){
         nav.style.display = "block";
+        
         
     }else{
         nav.style.display == "none";
